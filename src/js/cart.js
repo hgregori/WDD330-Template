608da86996b1067e13331e11c09839ae7268b4bf
Hgregori <Hgregorio@byupathway.edu>
2026-07-13 18:46:57 -0300

    w1-addAremoveButton

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,15 @@
-import { getLocalStorage, normalizeCartItems } from './utils.mjs';
+import {
+  getLocalStorage,
+  normalizeCartItems,
+  setLocalStorage,
+} from './utils.mjs';
+
+function removeFromCart(itemId) {
+  const cartItems = normalizeCartItems(getLocalStorage('so-cart'));
+  const updatedCart = cartItems.filter((item) => item.Id !== itemId);
+  setLocalStorage('so-cart', updatedCart);
+  renderCartContents();
+}
 
 function renderCartContents() {
   const cartItems = normalizeCartItems(getLocalStorage('so-cart'));
@@ -7,11 +18,18 @@ function renderCartContents() {
 
   if (productList) {
     productList.innerHTML = htmlItems.join('');
+
+    productList.querySelectorAll('.cart-card__remove').forEach((button) => {
+      button.addEventListener('click', () => {
+        removeFromCart(button.dataset.id);
+      });
+    });
   }
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <button class="cart-card__remove" data-id="${item.Id}" aria-label="Remove ${item.Name}">X</button>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -21,7 +39,7 @@ function cartItemTemplate(item) {
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__color">${item.Colors?.[0]?.ColorName || 'Color unavailable'}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
