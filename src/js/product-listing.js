775d2ed1f55ce77df46245168e434b9b4a5b9cc9
Hgregori <Hgregorio@byupathway.edu>
2026-07-23 19:28:14 -0300

    w4-teamactivity

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,16 +1,12 @@
-import { getParam, loadHeaderFooter } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductList from './productList.mjs';
-import Alert from './Alert.js';
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ExternalServices from "./ExternalService.mjs";
+import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
 
 const category = getParam("category");
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
 const listing = new ProductList(category, dataSource, element);
 
 listing.init();
-
-const alerts = new Alert();
-alerts.init();
